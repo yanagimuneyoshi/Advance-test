363b76ec2c0867bf938ce1d4a08a5b60c2d574d9
yanagimuneyoshi <yanagimuneyoshi1994gmail.com>
2023-05-24 21:37:36 +0900

    2023/5/24_21:37

--- src/resources/views/category.blade.php
+++ src/resources/views/category.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 
 <head>
   <meta charset="UTF-8">
@@ -15,26 +15,23 @@
     <div class="contact-form__heading">
       <h2>管理システム</h2>
     </div>
-
     <form class="form">
       <div class="form__group">
-        <div class="form__group-title">
-          <span class="form__label--item">お名前</span>
+        <div class="form__group-title--name">
+          <span class="form__label--item--name">お名前</span>
         </div>
         <div class="form__group-content">
           <div class="form__input--text--name">
             <input type="text" name="family-name" placeholder="テスト太郎" />
-            <input type="text" name="given-name" autocomplete="given-name" />
           </div>
         </div>
       </div>
-
       <div class="form__group">
-        <div class="form__group-title">
+        <div class="form__group-title--gender">
           <span class="form__label--item">性別</span>
         </div>
         <div class="form-body">
-          <div class="form-man">
+          <div class="form-man--all">
             <input type="radio" name="gender" value="全て" checked />
             <span class="form-sex-txt-all">全て</span>
           </div>
@@ -51,19 +48,19 @@
         </div>
       </div>
       <div class="form__group">
-        <div class="form__group-title">
+        <div class="form__group-title--day">
           <span class="form__label--item">登録日</span>
         </div>
         <div class="form__group-content">
           <div class="form__input--text--datetime">
             <input type="datetime-local	" name="datetime-local" />
-            <span class="form__input--text--datetime">〜</span>
+            <span class="form__input--text--line">〜</span>
             <input type="datetime-local	" name="datetime-local" />
           </div>
         </div>
       </div>
       <div class="form__group">
-        <div class="form__group-title">
+        <div class="form__group-title--email">
           <span class="form__label--item">メールアドレス</span>
         </div>
         <div class="form__group-content">
@@ -75,9 +72,47 @@
       <div class="form__button">
         <button class="form__button-submit" type="submit">検索</button>
       </div>
+      <form class="form__button__fix" action="/" method="post">
+        <div class="form__button__fix-reset">
+          <button class="form__button__fix-submit" type="submit">リセット</button>
+        </div>
+      </form>
     </form>
-    <form class="form__button__fix">
-      <button class="form__button__fix-submit" type="submit">リセット</button>
+
+
+    <form class="category-search">
+      <div class="category-table">
+        <table class="category-table__inner">
+          <tr class="category-table__row">
+            <th class="category-table__header__ID">ID</th>
+            <th class="category-table__header__name">お名前</th>
+            <th class="category-table__header__gender">性別</th>
+            <th class="category-table__header__email">メールアドレス</th>
+            <th class="category-table__header__opinion">ご意見</th>
+          </tr>
+
+          <tr class="category-table__row">
+            <td class="category-table__item">
+              <form class="update-form">
+                <div class="update-form__item">
+                  <input class="update-form__item-input" type="text">
+                  <input class="update-form__item-input" type="text">
+                  <input class="update-form__item-input" type="text">
+                  <input class="update-form__item-input" type="address">
+                  <input class="update-form__item-input" type="text">
+                </div>
+              </form>
+            </td>
+            <td class="category-table__item">
+              <form class="delete-form">
+                <div class="delete-form__button">
+                  <button class="delete-form__button-submit" type="submit">削除</button>
+                </div>
+              </form>
+            </td>
+          </tr>
+        </table>
+      </div>
     </form>
   </div>
 </main>
